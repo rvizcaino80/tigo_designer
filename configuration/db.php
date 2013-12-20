101a4c3adbfa11c9d7637539876156e34b2a0fc7
rvizcaino80 <rvizcaino@gmail.com>
2013-12-19 21:49:01 -0500

    Update db.php

--- configuration/db.php
+++ configuration/db.php
@@ -4,11 +4,5 @@ Configuration File
 Rogers Vizcaino - rvizcaino@gmail.com
 */
 
-//FREDYS MERCADO / ROGERS VIZCAINO
-
 //DB CONFIGURATION ARRAY
-if ($_SERVER['SERVER_NAME'] == 'localhost'){	
-	$dbMysql = array("server"=>"127.0.0.1", "user"=>"root", "password"=>"", "name"=>"designer");
-}else{	
-	$dbMysql = array("server"=>"tunnel.pagodabox.com", "user"=>"lasandra", "password"=>"JQnpTFZi", "name"=>"zeus");
-}
+$dbMysql = array("server"=>"127.0.0.1", "user"=>"root", "password"=>"", "name"=>"designer");
